c3b26f29636f3c67307719edf621d472d9b0d42c
TeckHuaNg <200348264@student.georgianc.on.ca>
2017-09-21 14:16:31 -0400

    first commit

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,40 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            //create a typed list of Genres
+            var genres = new List<Genre>();
+
+            //create 10 Genre in memory to pass to the main store view
+            for(int i = 1;i <= 10; i++)
+            {
+                genres.Add(new Genre { Name = "Genre" + i.ToString() });
+            }
+            //pass the typed list to the view for display
+            return View(genres);
+        }
+
+        // GET: Store/Browse
+        public ActionResult Browse(string genre)
+        {
+            ViewBag.Genre = genre;
+            return View();
+        }
+
+        public String Details(int id)
+        {
+            string message = "Store Details, ID = " + id;
+            return message;
+        }
+    }
+}
